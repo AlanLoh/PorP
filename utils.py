44b3c7bc0a950d59a2bf09f84c8f4dc29c147ff6
Alan Loh <alan.loh.up7@gmail.com>
2024-01-10 10:12:27 +0100

    make it more random

--- utils.py
+++ utils.py
@@ -10,8 +10,7 @@ __all__ = [
     "generate_card_list_from_domain_dict"
 ]
 
-
-RANDOM_GENERATOR = np.random.default_rng(seed=42) # no seed = non-reproducible
+RANDOM_SEED = None # # no seed = non-reproducible
 
 class Card:
     def __init__(self, *symbols):
@@ -24,6 +23,8 @@ def gauss_2d(x, y, center_x=0, center_y=0, sigma_x=1, sigma_y=1):
 
 def generate_domain_matrix(matrix_side: int, n_domains: int) -> np.ndarray:
     
+    rdn = np.random.default_rng(seed=RANDOM_SEED)
+
     domains = np.arange(n_domains, dtype=int)
     
     # Spread the domain values among the map
@@ -40,25 +41,29 @@ def generate_domain_matrix(matrix_side: int, n_domains: int) -> np.ndarray:
     for i in range(matrix_side):
         for j in range(matrix_side):
             domain_probability = gauss_2d(i, j, center_x=domain_centers_x, center_y=domain_centers_y, sigma_x=sigma, sigma_y=sigma)
-            domain_map[i, j] = RANDOM_GENERATOR.choice(domains, size=1, p=domain_probability/domain_probability.sum())[0]
+            domain_map[i, j] = rdn.choice(domains, size=1, p=domain_probability/domain_probability.sum())[0]
 
     return domain_map
 
-def assign_int_to_matrix(matrix: np.ndarray, n_symbols: int) -> None:
+def assign_int_to_matrix(matrix: np.ndarray, n_symbols: int) -> np.ndarray:
+    rdn = np.random.default_rng(seed=RANDOM_SEED)
+
     # Assign a symbol for each domain, uniformely distributed among n_symbols
     n_domains = np.unique(matrix).size
     for domain_i in range(n_domains):
         domain_mask = matrix == domain_i
         domain_size = np.sum(domain_mask)
-        matrix[domain_mask] = domain_i + RANDOM_GENERATOR.choice(np.linspace(0.1, 0.9, n_symbols), size=domain_size)
+        matrix[domain_mask] = domain_i + rdn.choice(np.linspace(0.1, 0.9, n_symbols), size=domain_size)
     return matrix
 
-def assign_symbol_to_matrix(matrix: np.ndarray, symbols: dict) -> None:
+def assign_symbol_to_matrix(matrix: np.ndarray, symbols: dict) -> np.ndarray:
+    rdn = np.random.default_rng(seed=RANDOM_SEED)
+    
     n_domains = np.unique(matrix).size
     for domain_i in range(n_domains):
         domain_mask = matrix == str(domain_i)
         domain_size = np.sum(domain_mask)
-        matrix[domain_mask] = RANDOM_GENERATOR.choice(symbols[domain_i], size=domain_size)
+        matrix[domain_mask] = rdn.choice(symbols[domain_i], size=domain_size)
     return matrix
 
 def generate_card_list(n_symbols: int = 5, n_domains: int = 3, n_cards: int = 100, n_symbols_per_card: int = 5) -> List[Card]:
@@ -80,7 +85,7 @@ def generate_card_list_from_domain_dict(domain_dict: dict, n_cards: int = 100, n
     map_side = int(np.sqrt(n_cards))
 
     # Loop over n_symbols_per_card and generate as many maps
-    symbol_map = np.empty((map_side, map_side, n_symbols_per_card), dtype=str)
+    symbol_map = np.empty((map_side, map_side, n_symbols_per_card), dtype="U2")
     for i in range(n_symbols_per_card):
         domain_map = generate_domain_matrix(matrix_side=map_side, n_domains=len(domain_dict))
         domain_map = domain_map.astype(str)
